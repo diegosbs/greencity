6d271a5e5b49b8ec41fb9d50219fdd7e06bf6a91
Diego <diego_sbs2009@hotmaill.com>
2023-10-10 02:05:58 -0300

    fix sprite

--- _Models/Hero.cs
+++ _Models/Hero.cs
@@ -10,24 +10,24 @@ public class Hero
     {
         var heroTexture = Globals.Content.Load<Texture2D>("walkingtest");
 
-        _anims.AddAnimation(new Vector2(0,1), new(heroTexture, 8, 8, 0.1f, 1));
-        _anims.AddAnimation(new Vector2(-1,0), new(heroTexture, 8, 8, 0.1f, 2));
-        _anims.AddAnimation(new Vector2(1,0), new(heroTexture, 8, 8, 0.1f, 3));
-        _anims.AddAnimation(new Vector2(0,-1), new(heroTexture, 8, 8, 0.1f, 4));
-        _anims.AddAnimation(new Vector2(-1,1), new(heroTexture, 8, 8, 0.1f, 5));
-        _anims.AddAnimation(new Vector2(-1,-1), new(heroTexture, 8, 8, 0.1f, 6));
-        _anims.AddAnimation(new Vector2(1,1), new(heroTexture, 8, 8, 0.1f, 7));
-        _anims.AddAnimation(new Vector2(1,-1), new(heroTexture, 8, 8, 0.1f, 8));
+        _anims.AddAnimation(new Vector2(0, -1), new(heroTexture, 8, 4, 0.1f, 1));
+        _anims.AddAnimation(new Vector2(-1, 0), new(heroTexture, 8, 4, 0.1f, 2));
+        _anims.AddAnimation(new Vector2(0, 1), new(heroTexture, 8, 4, 0.1f, 3));
+        _anims.AddAnimation(new Vector2(1, 0), new(heroTexture, 8, 4, 0.1f, 4));
+        _anims.AddAnimation(new Vector2(1, -1), new(heroTexture, 8, 4, 0.1f, 4));
+        _anims.AddAnimation(new Vector2(1, 1), new(heroTexture, 8, 4, 0.1f, 4));
+        _anims.AddAnimation(new Vector2(-1, -1), new(heroTexture, 8, 4, 0.1f, 2));
+        _anims.AddAnimation(new Vector2(-1, 1), new(heroTexture, 8, 4, 0.1f, 2));
     }
 
     public void Update()
     {
         if(InputManager.Moving)
         {
-            _position += Vector2.Normalize(InputManager.DirectionArrows) * _speed * Globals.TotalSeconds;
+            _position += Vector2.Normalize(InputManager.Direction) * _speed * Globals.TotalSeconds;
         }
 
-        _anims.Update(InputManager.DirectionArrows);
+        _anims.Update(InputManager.Direction);
     }
 
     public void Draw()
